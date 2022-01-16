709ef169d050bd2abb317e83825fbe08563b52d1
oopty <tiramisu13795@gmail.com>
2022-01-16 20:57:09 +0900

    start.spring.io로 초기 프로젝트 생성 아래 의존성을 가짐  - H2 database  - lombok  - spring web

---
+++ src/main/java/io/oopty/downbit/DownbitApplication.java
@@ -0,0 +1,13 @@
+package io.oopty.downbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DownbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DownbitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/oopty/downbit/DownbitApplicationTests.java
@@ -0,0 +1,13 @@
+package io.oopty.downbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DownbitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
