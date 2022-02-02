1dee7a59fab74b6719b04e3eddcf5dff5e8c46ce
oopty <tiramisu13795@gmail.com>
2022-02-02 16:54:22 +0900

    :necktie: 주문 생성 API 비지니스 로직 TDD 수행
    - cycle1 (RED)

--- src/main/java/io/oopty/downbit/order/OrderVO.java
+++ src/main/java/io/oopty/downbit/order/OrderVO.java
@@ -1,5 +1,25 @@
 package io.oopty.downbit.order;
 
-public class OrderVO {
+import lombok.*;
 
+import java.time.LocalDateTime;
+
+@Builder(builderMethodName = "OrderVOBuilder")
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@ToString
+@Getter
+public class OrderVO {
+    int id;
+    String uuid;
+    int currency;
+    int user;
+    String side;
+    String type;
+    double avgPrice;
+    String state;
+    LocalDateTime dateTime;
+    double volume;
+    double executedVolume;
+    double remainingVolume;
+    int tradeCount;
 }
---
+++ src/test/java/io/oopty/downbit/order/OrderServiceTest.java
@@ -0,0 +1,51 @@
+package io.oopty.downbit.order;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+@ExtendWith(MockitoExtension.class)
+class OrderServiceTest {
+    public static final int BTC_CURRENCY_ID = 123;
+    public static final int USER_ID = 456;
+    public static final String BID = "bid";
+    public static final String PRICE = "price";
+    public static final String OPENED = "opened";
+    public static final int ETH_CURRENCY_ID = 234;
+
+    @Spy
+    private OrderService orderService;
+
+    @Nested
+    class MarketOrderTest {
+
+        @Test
+        void whenSubmitMarketOrderForBTCCreateAndReceiveOrderInfo() {
+            OrderVO result = orderService.order("BTC/KRW", BID, PRICE, 0,  100);
+
+            assertThat(result.getCurrency(), is(BTC_CURRENCY_ID));
+            assertThat(result.getUser(), is(USER_ID));
+            assertThat(result.getSide(), is(BID));
+            assertThat(result.getType(), is(PRICE));
+            assertThat(result.getAvgPrice(), closeTo(100, 0.00001));
+            assertThat(result.getState(), is(OPENED));
+            assertThat(result.getDateTime(), any(LocalDateTime.class));
+            assertThat(result.getVolume(), closeTo(100, 0.00001));
+            assertThat(result.getExecutedVolume(), closeTo(0, 0.00001));
+            assertThat(result.getRemainingVolume(), closeTo(100, 0.00001));
+            assertThat(result.getTradeCount(), is(0));
+        }
+    }
+    
+    @Nested 
+    class LimitOrderTest {
+
+    }
+}
