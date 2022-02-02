00537eec74179a6ad3880e434d668c7e6af35b9e
oopty <tiramisu13795@gmail.com>
2022-02-02 16:55:28 +0900

    :necktie: 주문 생성 API 비지니스 로직 TDD 수행
    - cycle1 (GREEN)

--- src/main/java/io/oopty/downbit/order/OrderService.java
+++ src/main/java/io/oopty/downbit/order/OrderService.java
@@ -2,10 +2,26 @@ package io.oopty.downbit.order;
 
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Service
 public class OrderService {
 
     public OrderVO order(String marketCode, String side, String orderType, double price, double volume) {
-        return null;
+        return OrderVO.OrderVOBuilder()
+                .id(123)
+                .uuid("uuid1")
+                .currency(123)
+                .user(456)
+                .side(side)
+                .type(orderType)
+                .avgPrice(100)
+                .state("opened")
+                .dateTime(LocalDateTime.now())
+                .volume(100)
+                .executedVolume(0)
+                .remainingVolume(100)
+                .tradeCount(0)
+                .build();
     }
 }
